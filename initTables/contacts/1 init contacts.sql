8729faf0f7d752ad3a437fc71b138a4f91441177
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:09:14 -0400

    Delete 1 init contacts.sql

--- initTables/contacts/1 init contacts.sql
+++
@@ -1,2 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\contacts/createTable contacts.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\contacts/grantRightsToRoles contacts.sql'
