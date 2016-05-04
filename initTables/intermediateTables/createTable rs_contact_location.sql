aaf6216d33bbc3926643b89e2afecd8fb2da886e
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:10:44 -0400

    Delete createTable rs_contact_location.sql

--- initTables/intermediateTables/createTable rs_contact_location.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE rs_contact_location
-(
-contact_Id		CHAR(10) 	NOT NULL REFERENCES contacts(contact_Id),
-location_Id		CHAR(5) 	NOT NULL REFERENCES locationList(location_Id),
-CONSTRAINT rscl_Id PRIMARY KEY (contact_Id, location_Id)			
-) TABLESPACE radiosilence
-/
