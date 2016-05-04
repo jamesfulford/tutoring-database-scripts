23859033066b4bbf0b6e0192f97fa819b86ad430
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:09:42 -0400

    Delete grantRightsToRoles expenseList.sql

--- initTables/expenseList/grantRightsToRoles expenseList.sql
+++
@@ -1,6 +0,0 @@
-GRANT ALL ON  expenseList TO accountant;
-GRANT SELECT ON expenseList TO administrator;
-GRANT SELECT ON expenseList TO marketer;
-GRANT SELECT ON expenseList TO researcher;
-GRANT SELECT ON expenseList TO tutor;
-
