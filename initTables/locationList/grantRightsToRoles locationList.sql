eb8e35834d745e38f4a583de99e130e67d77ba5a
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:11:04 -0400

    Delete grantRightsToRoles locationList.sql

--- initTables/locationList/grantRightsToRoles locationList.sql
+++
@@ -1,6 +0,0 @@
-GRANT SELECT ON locationList TO accountant;
-GRANT ALL ON locationList TO administrator;
-GRANT SELECT ON locationList TO marketer;
-GRANT SELECT ON locationList TO researcher;
-GRANT SELECT ON locationList TO tutor;
-
