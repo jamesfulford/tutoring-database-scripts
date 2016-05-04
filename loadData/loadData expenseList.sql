81d7664ae05a191a1d85358102b6050276b01921
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:13:39 -0400

    Delete loadData expenseList.sql

--- loadData/loadData expenseList.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO expenseList (expenseList_Id, type, description) values ('E008',  'Advertising' ,  'Business cards' );
-INSERT INTO expenseList (expenseList_Id, type, description) values ('E009',  'Mileage' ,  'quantity should be number of miles' );
-INSERT INTO expenseList (expenseList_Id, type, description) values ('E011',  'W9 Employees' ,  'Accountant' );
-INSERT INTO expenseList (expenseList_Id, type, description) values ('E018',  'Office Supplies' ,  'pens and such' );
-INSERT INTO expenseList (expenseList_Id, type, description) values ('E24b',  'Meals' ,  'non-extravagent, related to business' );
