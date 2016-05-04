7f49724bacb478b6319f6862c5563ac3f6317cdc
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:11:41 -0400

    Delete 1 init subjectList.sql

--- initTables/subjectList/1 init subjectList.sql
+++
@@ -1,2 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\subjectList/createTable subjectList.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\subjectList/grantRightsToRoles subjectList.sql'
