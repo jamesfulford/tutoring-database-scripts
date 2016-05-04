9e6135619fad6f91d4812f8d5c7d0436c6e04104
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:09:49 -0400

    Delete 1 init expenses.sql

--- initTables/expenses/1 init expenses.sql
+++
@@ -1,2 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\expenses/createTable expenses.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\expenses/grantRightsToRoles expenses.sql'
