7e47ad14697e7669ecd0eca21082320729356877
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:09:33 -0400

    Delete 1 init expenseList.sql

--- initTables/expenseList/1 init expenseList.sql
+++
@@ -1,2 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\expenseList/createTable expenseList.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\expenseList/grantRightsToRoles expenseList.sql'
