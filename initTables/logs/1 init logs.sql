0b1fb3a40c042a4570911313d93936d62ac753de
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:11:10 -0400

    Delete 1 init logs.sql

--- initTables/logs/1 init logs.sql
+++
@@ -1,2 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\logs/createTable logs.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\logs/grantRightsToRoles logs.sql'
