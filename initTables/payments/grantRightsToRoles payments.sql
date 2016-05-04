f405c270eac331f92155f45e46b6fb4265d563b9
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:11:34 -0400

    Delete grantRightsToRoles payments.sql

--- initTables/payments/grantRightsToRoles payments.sql
+++
@@ -1,6 +0,0 @@
-GRANT ALL ON payments TO accountant;
-GRANT SELECT, UPDATE, INSERT, DELETE ON payments TO administrator;
-GRANT SELECT ON payments TO marketer;
-GRANT SELECT ON payments TO researcher;
-GRANT SELECT, INSERT ON payments TO tutor;
-
