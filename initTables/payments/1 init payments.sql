84dbc9fd5e76a800c26e252f01fb763ecc948542
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:11:26 -0400

    Delete 1 init payments.sql

--- initTables/payments/1 init payments.sql
+++
@@ -1,2 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\payments/createTable payments.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\payments/grantRightsToRoles payments.sql'
