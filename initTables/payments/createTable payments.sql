3d975e9620edfcccbed9063c6ae0120c83851640
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:11:30 -0400

    Delete createTable payments.sql

--- initTables/payments/createTable payments.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE payments
-( 
-	payment_Id 	CHAR(13) 	NOT NULL	PRIMARY KEY,
-	datePaid 	DATE				,
-	amount	 	NUMBER(*, 2)			,
-	contact_Id 	CHAR(10)	NOT NULL REFERENCES contacts(contact_Id)	
-) TABLESPACE radiosilence
-/
