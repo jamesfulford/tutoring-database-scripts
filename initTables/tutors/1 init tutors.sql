a29342ec4c76608f15bd9d464a60481ace2f82bd
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:11:56 -0400

    Delete 1 init tutors.sql

--- initTables/tutors/1 init tutors.sql
+++
@@ -1,2 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables/tutors/createTable tutors.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initTables\tutors/grantRightsToRoles tutors.sql'
