1080a009d357ae02bdc2356a7523793a09a321de
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:08:27 -0400

    Delete 1 initAllRoles.sql

--- initRoles/1 initAllRoles.sql
+++
@@ -1,6 +0,0 @@
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initRoles/accountant.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initRoles/administrator.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initRoles/gateKeeper.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initRoles/marketer.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initRoles/researcher.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence\initRoles/tutor.sql'
