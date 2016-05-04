663bd7e63d7acec65b5e200b8ffbaa65b5b7ae2d
James P. Fulford <Semimajor@users.noreply.github.com>
2016-05-04 00:16:08 -0400

    Delete radiosilence tablespaceInit.sql

--- radiosilence tablespaceInit.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLESPACE radiosilence DATAFILE 'C:\app\James\oradata\orcl/radiosilence.dbf' SIZE 500M EXTENT MANAGEMENT LOCAL AUTOALLOCATE;
-
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence/initRoles/1 initAllRoles.sql'
-@'C:\Users\James\Desktop\Oracle\tutoringSchema\radiosilence/initUsers/1 initAllUsers.sql'
